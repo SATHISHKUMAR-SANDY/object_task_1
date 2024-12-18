0ffc93cf2e584364cc8737bf205bfb0b91fbc9d2
SATHISHKUMAR.M <sathish323334@gmail.com>
2024-12-18 15:48:08 +0530

    sathish

--- object_tasks.js
+++ object_tasks.js
@@ -1,36 +1,36 @@
 // //TASK 1
 
-// let book = {
-//     name:"ATTOMIC HABBITS",
-//     authour:"JAMSCLEAR",
-//     relese:1999
-// }
-// console.log(book);
+let book = {
+    name:"ATTOMIC HABBITS",
+    authour:"JAMSCLEAR",
+    relese:1999
+}
+console.log(book);
 
-// book.authour = "sathish";
-// console.log(book);
-// book["relese"] = 2024;
-// console.log(book); 
+book.authour = "sathish";
+console.log(book);
+book["relese"] = 2024;
+console.log(book); 
 
 
-//TASK 2
+// TASK 2
 
-// let book_2 = {
-//     name:"ATTOMIC HABBITS",
-//     authour:"JAMSCLEAR",
-//     relese:1999,
-//     book_2_summary(){
-//         console.log("Book name is:"+this.name+" "+"AUTHOUR NAME IS:"+this.authour);
-//     }
-// }
-// book_2.book_2_summary()
+let book_2 = {
+    name:"ATTOMIC HABBITS",
+    authour:"JAMSCLEAR",
+    relese:1999,
+    book_2_summary(){
+        console.log("Book name is:"+this.name+" "+"AUTHOUR NAME IS:"+this.authour);
+    }
+}
+book_2.book_2_summary()
 
 
 //TASK 3
 
-// let {name,relese} = book_2;
-// console.log(name);
-// console.log(relese);
+let {name,relese} = book_2;
+console.log(name);
+console.log(relese);
 
 // //TASK 4
 let book_3 = {
